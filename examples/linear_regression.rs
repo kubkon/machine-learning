e0b6af58f7c71b8032113ad087122c911350112b
Jakub Konka <jakub.konka@strath.ac.uk>
2015-02-07 12:26:17 +0000

    Change tolerance level for the example.

--- examples/linear_regression.rs
+++ examples/linear_regression.rs
@@ -6,7 +6,7 @@ use machinelearning::linear_regression::LinearRegression;
 fn main() {
     let xs = Vector::from_slice(&[1.0,2.0,4.0,0.0]);
     let ys = Vector::from_slice(&[0.5,1.0,2.0,0.0]);
-    let mut model = LinearRegression::new(0.1, 1e-6);
+    let mut model = LinearRegression::new(0.1, 1e-8);
     model.fit(&xs, &ys);
     println!("{:?}", model);
 }
